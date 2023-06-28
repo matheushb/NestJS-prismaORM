9b53e16a24e05d56e4231582795165295f2c5204
Matheus <matheushbaraldi@gmail.com>
2023-06-28 18:54:15 -0300

    feat: user login/signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UserSignupDto } from './dto/user-signup.dto';
 import { UserLoginDto } from './dto/user-login.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly prismaService: PrismaService) {}
-  login(userLoginDto: UserLoginDto) {
-    this.prismaService.$queryRaw`SELECT * FROM User`;
+  async login(userinfo: UserLoginDto) {
+    const user = await this.prismaService.user.findFirst({
+      where: { email: userinfo.email },
+    });
+    if (!user) throw new NotFoundException('Credentials dont match');
+
+    const passwordCorrect = await bcrypt.compare(
+      userinfo.password,
+      user.password,
+    );
+    if (!passwordCorrect) throw new NotFoundException('Credentials dont match');
+
+    return user;
   }
 
-  signup(userdata: UserSignupDto) {
-    return userdata;
+  async signup(userdata: UserSignupDto) {
+    userdata.password = bcrypt.hashSync(userdata.password, 12);
+    try {
+      const newUser = await this.prismaService.user.create({
+        data: {
+          email: userdata.email,
+          name: userdata.name,
+          password: userdata.password,
+          age: userdata.age,
+        },
+      });
+      delete newUser.password;
+      return newUser;
+    } catch (error) {
+      if (error.code === 'P2002') {
+        throw new ForbiddenException('Email already exists');
+      }
+      throw error;
+    }
   }
 }
